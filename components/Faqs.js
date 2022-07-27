60ed6e1d4464554c47d129406ab3e194858f1d2c
teguhdarma <78896400+teguhdarma@users.noreply.github.com>
2022-07-27 14:33:49 +0900

    the news

--- components/Faqs.js
+++ components/Faqs.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { useState } from 'react';
 
@@ -44,33 +45,35 @@ const Item = ({ title, children }) => {
 
 export const Faqs = () => {
   return (
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
-        <div class="flex flex-col mb-16 sm:text-center">
-          <a href="/" class="mb-6 sm:mx-auto">
-            <div class="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
-              <svg
-                class="w-10 h-10 text-deep-purple-accent-400"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
-              >
-                <polygon
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
-                />
-              </svg>
-            </div>
-          </a>
-          <div class="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
-            <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-              <span class="relative inline-block">
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+        <div className="flex flex-col mb-16 sm:text-center">
+          <div className="mb-6 sm:mx-auto">
+            <Link href="/">
+              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+                <svg
+                  className="w-10 h-10 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </Link>
+          </div>
+          <div className="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+              <span className="relative inline-block">
                 <svg
                   viewBox="0 0 52 24"
                   fill="currentColor"
-                  class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                  className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
                 >
                   <defs>
                     <pattern
@@ -89,17 +92,17 @@ export const Faqs = () => {
                     height="24"
                   />
                 </svg>
-                <span class="relative">The</span>
+                <span className="relative">The</span>
               </span>{' '}
               quick, brown fox jumps over a lazy dog
             </h2>
-            <p class="text-base text-gray-700 md:text-lg">
+            <p className="text-base text-gray-700 md:text-lg">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque rem aperiam, eaque ipsa quae.
             </p>
           </div>
         </div>
-        <div class="space-y-4">
+        <div className="space-y-4">
           <Item title="The quick, brown fox jumps over a lazy dog?">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque rem aperiam, eaque ipsa quae.

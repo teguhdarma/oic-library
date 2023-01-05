f868f4e975d143eee5c449090e3336ab54dabacc
teguhdarma <78896400+teguhdarma@users.noreply.github.com>
2023-01-06 01:05:24 +0900

    my changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head';
 import React from 'react';
-import { Slide, Product, Simple } from '../components';
 import { client, urlFor } from '../lib/client';
 import Features from '../components/Features';
-import { Team } from '../components/Team';
-import { TwitterTweetEmbed, TwitterTimelineEmbed } from 'react-twitter-embed';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import New from '../components/New';
 import Table from './Table';
 import Maps from '../components/Maps';
@@ -13,7 +11,7 @@ import Hero from '../components/Hero';
 import Crisasi from '../components/Crisasi';
 import Link from 'next/link';
 
-const Home = ({ products, bannerData, slides, posts }) => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
       <Head>
@@ -69,9 +67,9 @@ const Home = ({ products, bannerData, slides, posts }) => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </a>
